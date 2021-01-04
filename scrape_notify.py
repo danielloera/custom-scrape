5e399452dffcf9736b127d6e7b82363c02c684c3
Daniel Loera <danny.reatret@gmail.com>
2021-01-04 10:57:34 -0800

    Add json file configs for WebScrapeConfig

--- scrape_notify.py
+++ scrape_notify.py
@@ -1,16 +1,7 @@
 from src.scrape import SeleniumScraper
 from src.website_scrape_config import WebsiteScrapeConfig
 
-amiami_config = WebsiteScrapeConfig(
-    name="Amiami Hatsune Miku Figure Preorders",
-    urls=[
-    # Bishoujo
-    'https://www.amiami.com/eng/search/list/?s_keywords=Hatsune%20Miku&s_st_list_preorder_available=1&s_cate_tag=14',
-    # Plastic models
-    'https://www.amiami.com/eng/search/list/?s_keywords=Hatsune%20Miku&s_st_list_preorder_available=1&s_cate_tag=36'],
-    wait_for_class='new-items',
-    item_class='newly-added-items__item'
-)
-
+configs = WebsiteScrapeConfig.listFromFile('scrape_configs.json')
 scraper = SeleniumScraper()
-scraper.scrape_and_screenshot_urls(amiami_config)
+for config in configs:
+    url_to_screenshots_map = scraper.scrape_and_screenshot_urls(config)
--- src/website_scrape_config.py
+++ src/website_scrape_config.py
@@ -1,3 +1,6 @@
+import json
+
+
 class WebsiteScrapeConfig:
 
     def __init__(self, name=None, urls=None, wait_for_class=None, item_class=None):
@@ -14,3 +17,24 @@ class WebsiteScrapeConfig:
 
     def __hash__(self):
         return hash(self.__key())
+
+    @staticmethod
+    def listFromFile(config_file_name):
+        with open(config_file_name) as f:
+            json_configs = json.load(f)
+            configs = []
+            for json_config in json_configs:
+                name = json_config.get("name", None)
+                urls = json_config.get("urls", None)
+                wait_for_class = json_config.get("wait_for_class", None)
+                item_class = json_config.get("item_class", None)
+                if name is None:
+                    raise ValueError('Each scrape config needs a name.')
+                if urls is None or not urls:
+                    raise ValueError(
+                        'Each scrape config needs at least 1 url.')
+                if item_class is None:
+                    raise ValueError('Each scrape config needs an item_class.')
+                configs.append(WebsiteScrapeConfig(
+                    name=name, urls=urls, wait_for_class=wait_for_class, item_class=item_class))
+        return configs
