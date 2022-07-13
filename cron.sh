e994dd9c35eda59bf2593b4d9c3f88d514e0a778
Daniel Loera <danny.reatret@gmail.com>
2022-07-13 16:23:26 -0500

    Update gitignore and remove personal files, oops

--- cron.sh
+++
@@ -1,8 +0,0 @@
-printf "Pulling latest changes...\n"
-
-git -C custom-scrape pull
-
-printf "\n"
-
-python3 custom-scrape/custom_scrape.py --discord_notification_channel general --config_file "custom-scrape/daniel_configs.json"
-
